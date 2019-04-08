74f41f6aad1317d3e20c561afa86d221bb965be7
Peter Hoggarth <hoggarth.peter@gmail.com>
2019-04-08 17:46:16 -0400

    Changed fonts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -18,13 +18,14 @@ class BlogPostTemplate extends React.Component {
           title={post.frontmatter.title}
           description={post.frontmatter.description || post.excerpt}
         />
-        <h1>{post.frontmatter.title}</h1>
+        <h1 style={{fontFamily: "Roboto"}}>{post.frontmatter.title}</h1>
         <p
           style={{
             ...scale(-1 / 5),
             display: `block`,
             marginBottom: rhythm(1),
             marginTop: rhythm(-1),
+            fontFamily: "Roboto"
           }}
         >
           {post.frontmatter.date}
